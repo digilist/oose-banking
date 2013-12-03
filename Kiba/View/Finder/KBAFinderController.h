6ecaf05f88968d931e1cebbf9d706b0cf8e27f72
Markus Fasselt <markus.fasselt@gmail.com>
2013-12-03 08:43:59 +0100

    Filialfinder Controller

---
+++ Kiba/View/Finder/KBAFinderController.h
@@ -0,0 +1,13 @@
+//
+//  KBAFinanceController.h
+//  Kiba
+//
+//  Created by 1fasselt on 03.12.13.
+//  Copyright (c) 2013 KiBa App. All rights reserved.
+//
+
+#import "KBAViewController.h"
+
+@interface KBAFinderController : KBAViewController
+
+@end
---
+++ Kiba/View/Finder/KBAFinderController.m
@@ -0,0 +1,29 @@
+//
+//  KBAFinanceController.m
+//  Kiba
+//
+//  Created by 1fasselt on 03.12.13.
+//  Copyright (c) 2013 KiBa App. All rights reserved.
+//
+
+#import "KBAFinderController.h"
+
+@interface KBAFinderController ()
+
+@end
+
+@implementation KBAFinderController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
