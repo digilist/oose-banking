f9521be49279a8fbb5ba4fe74d848e27bea3cb78
1jendryc <1jendryc@rzmac29.informatik.uni-hamburg.de>
2013-12-05 11:11:09 +0100

    account service created

---
+++ Kiba/Services/AccountService.h
@@ -0,0 +1,14 @@
+//
+//  AccountService.h
+//  Kiba
+//
+//  Created by 1jendryc on 05.12.13.
+//  Copyright (c) 2013 KiBa App. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AccountService : NSObject
+
+
+@end
---
+++ Kiba/Services/AccountService.m
@@ -0,0 +1,13 @@
+//
+//  AccountService.m
+//  Kiba
+//
+//  Created by 1jendryc on 05.12.13.
+//  Copyright (c) 2013 KiBa App. All rights reserved.
+//
+
+#import "AccountService.h"
+
+@implementation AccountService
+
+@end
