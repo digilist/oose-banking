a1a55a6752efe6b55f740bf4611109a1064d3df1
Schaarschmidt <xschaars@rzmac30.informatik.uni-hamburg.de>
2014-01-14 15:57:39 +0100

    Added About View

---
+++ Kiba/View/About/KBAAbout.h
@@ -0,0 +1,13 @@
+//
+//  KBAAbout.h
+//  Kiba
+//
+//  Created by Schaarschmidt, Kevin Michael on 14.01.14.
+//  Copyright (c) 2014 KiBa App. All rights reserved.
+//
+
+#import "KBAViewController.h"
+
+@interface KBAAbout : KBAViewController
+
+@end
