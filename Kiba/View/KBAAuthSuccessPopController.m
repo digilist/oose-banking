003f7e050c37b539ce3db0f1c12bac8f6cdc4596
Schaarschmidt <xschaars@rzmac30.informatik.uni-hamburg.de>
2014-01-14 12:37:45 +0100

    Added auth success view

---
+++ Kiba/View/KBAAuthSuccessPopController.m
@@ -0,0 +1,38 @@
+//
+//  KBAAuthSuccessPopController.m
+//  Kiba
+//
+//  Created by Schaarschmidt, Kevin Michael on 14.01.14.
+//  Copyright (c) 2014 KiBa App. All rights reserved.
+//
+
+#import "KBAAuthSuccessPopController.h"
+
+@interface KBAAuthSuccessPopController ()
+
+@end
+
+@implementation KBAAuthSuccessPopController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:@"KBAAuthSuccessPopController" bundle:nibBundleOrNil];
+    if (self) {
+        self.preferredContentSize = self.view.frame.size;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
